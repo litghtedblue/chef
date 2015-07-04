dba6e628ff8bafa1dec9611afe794d98f77dd902
lightedblue <root@v157-7-242-172.myvps.jp>
2015-07-04 22:52:11 +0900

    for glassfish

---
+++ cookbooks/glassfish/metadata.rb
@@ -0,0 +1,14 @@
+name             'glassfish'
+maintainer       'Peter Donald'
+maintainer_email 'peter@realityforge.org'
+license          'Apache 2.0'
+description      'Installs/Configures GlassFish Application Server'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.6.0'
+
+supports 'ubuntu'
+
+depends 'java'
+depends 'authbind'
+depends 'cutlery'
+recommends 'runit'
